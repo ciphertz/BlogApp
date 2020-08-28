e4df8afa272945d0f6460dc1c87e917b3a0966dc
cipher <fynhacker@gmail.com>
2020-08-28 15:48:29 +0300

    removing some comments

--- blog/views.py
+++ blog/views.py
@@ -10,23 +10,6 @@ from django.views.generic import (
 )
 from .models import Post
 
-# Create your views here.
-
-# posts = [
-#     {
-#         'author': 'CoreyMS',
-#         'title': 'Blog Post 1',
-#         'content': 'First post content',
-#         'date_posted': 'August 27, 2018'
-#     },
-#     {
-#         'author': 'Jane Doe',
-#         'title': 'Blog Post 2',
-#         'content': 'Second post content',
-#         'date_posted': 'August 28, 2018'
-#     }
-# ]
-
 
 def home(request):
     context = {
